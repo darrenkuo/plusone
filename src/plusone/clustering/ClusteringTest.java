7c5812d95fee9b6d7fd52ae51d44688925a728f0
James Cook <james.cook@utoronto.ca>
2011-09-28 21:43:23 -0700

    Whoops!  I added the comment to the wrong method before.

--- src/plusone/clustering/ClusteringTest.java
+++ src/plusone/clustering/ClusteringTest.java
@@ -7,18 +7,6 @@ public abstract class ClusteringTest implements ClusteringMethod {
 	this.testName = testName;
     }
 
-    /**
-     * Predict k words to occur in each document in the testing set.  The
-     * testing documents (and training documents, if any) should be provided to
-     * the ClusteringTest instance in some other way (e.g. passed to a
-     * constructor).
-     *
-     * @param k The number of distinct words to predict for each document.
-     * @param outputUsedWord If fals, this method should not list any words
-     *        that it has already seen in the document.
-     * @return p, where p[i][j] is the j-th predicted word for the i-th
-     *         document.
-     */
     public Integer[][] predict(int k, boolean outputUsedWord) {
 	return null;
     }
