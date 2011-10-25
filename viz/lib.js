e29326185d578de33fc2d43608c93e281223029f
James Cook <james.cook@utoronto.ca>
2011-10-25 09:13:49 -0700

    (Testing bubbles.)

--- viz/lib.js
+++ viz/lib.js
@@ -1,9 +1,9 @@
 var lib = (function(){
     var startMain = function() {
 	data = [];
-	for (var x = 0.0; x < 2 * Math.PI; x += 0.01)
-	    data.push([x, Math.sin(x)]);
-	$.plot($("#plot"), [data], {});
+	for (var x = 0.0; x < 2 * Math.PI; x += 0.1)
+	    data.push([x, Math.sin(x), 0.1 + 0.03 * Math.cos(x)]);
+	$.plot($("#plot"), [data], {series: {bubbles: {active: true, show: true}}});
     };
 
     return {
