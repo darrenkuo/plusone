e89d8255dabec0799c2684428e8dbbf22a88949b
dw236 <wangdi880727@gmail.com>
2011-10-28 13:32:26 -0700

    Update src/plusone/utils/PaperAbstract.java

--- src/plusone/utils/PaperAbstract.java
+++ src/plusone/utils/PaperAbstract.java
@@ -24,55 +24,65 @@ public class PaperAbstract implements TrainingPaper, PredictionPaper {
     private Map<Integer, Integer> tf;
     public double norm;
     
-    protected static Map<Integer, Integer> makeTf(Integer[] abstractWords) {
+  /*  protected static Map<Integer, Integer> makeTf(Integer[] abstractWords) {
 	Map<Integer, Integer> tf = new HashMap<Integer, Integer>();
 	for (Integer word : abstractWords) {
 	    if (!tf.containsKey(word))
-		tf.put(word, 0);
-	    tf.put(word, tf.get(word) + 1);
+	    	tf.put(word, 1);
+	    else
+	    	tf.put(word, tf.get(word) + 1);
 	}
 	return tf;
-    }
+    }*/
 
     public PaperAbstract(int index, Integer[] inReferences, 
 			 Integer[] outReferences, 
 			 Integer[] abstractWords) {
-	this(index, inReferences, outReferences, makeTf(abstractWords));
+    	this.index = index;
+    	this.inReferences = inReferences;
+    	this.outReferences = outReferences;
+    	this.tf = new HashMap<Integer, Integer>();
+    	for (Integer word : abstractWords) {
+    	    if (!tf.containsKey(word))
+    	    	tf.put(word, 1);
+    	    else
+    	    	tf.put(word, tf.get(word) + 1);
+    	}
     }
 
-    public PaperAbstract(int index, Integer[] inReferences,
+/*    public PaperAbstract(int index, Integer[] inReferences,
 			 Integer[] outReferences, Map<Integer, Integer> tf) {
 	this.index = index;
 	this.inReferences = inReferences;
 	this.outReferences = outReferences;
 	this.tf = tf;
-    }
+    }*/
 
     /**
      * Generates tf depending on training or testing.  This function
      * must be called before we can use this paper in clustering
      * methods.
      */
-    public void generateTf(double percentUsed, Terms.Term[] terms, 
+    public void generateTf(double testWordpercent, Term[] terms, 
 			   boolean test){
 	Random randGen = Main.getRandomGenerator();
 	trainingTf = new HashMap<Integer, Integer>();
 	testingTf = test ? new HashMap<Integer, Integer>() : null;
+	norm = 0;
 
 	for (Integer word : tf.keySet()) {
 	    if (terms != null) terms[word].addDoc(this, test);
-	    if (test && randGen.nextDouble() > percentUsed) {
-		testingTf.put(word, tf.get(word));
+	    int freq= tf.get(word);
+	    if (test && randGen.nextDouble() < testWordpercent) {
+		testingTf.put(word, freq);
 	    } else {
-		trainingTf.put(word, tf.get(word));
+		trainingTf.put(word, freq);
+		norm+=freq*freq;
 		if (!test && terms != null)
 		    terms[word].totalCount += tf.get(word);
 	    }
 	}
 
-    	for (Map.Entry<Integer, Integer> entry : trainingTf.entrySet()) {
-	    norm += entry.getValue() * entry.getValue();
-    	}
     	norm = Math.sqrt(norm);
     }
 
@@ -87,7 +97,7 @@ public class PaperAbstract implements TrainingPaper, PredictionPaper {
 
     public Integer getTestingTf(Integer word) {
 	return testingTf == null ? 0 : 
-	    (testingTf.get(word) == null? 0 : testingTf.get(word));
+	    (testingTf.containsKey(word)? 0 : testingTf.get(word));
     }
 
     public Set<Integer> getTestingWords() {
@@ -99,16 +109,14 @@ public class PaperAbstract implements TrainingPaper, PredictionPaper {
     public boolean isTest() { return testingTf != null; }
     
     public double similarity(PaperAbstract a){
-    	double dist = 0.0;
+    	double sim = 0.0;
 
 	for (Map.Entry<Integer, Integer> entry : trainingTf.entrySet()) {
 	    int wordId = entry.getKey();
 	    int count = entry.getValue();
-	    if (a.trainingTf.containsKey(wordId)) {
-		dist += count * a.trainingTf.get(wordId);
-	    }
+		sim += count * a.getTestingTf(wordId);
     	}
-    	return dist/(a.norm * norm);
+    	return sim/(a.getNorm() * norm);
     }
 
     public boolean equals(Object obj) {
