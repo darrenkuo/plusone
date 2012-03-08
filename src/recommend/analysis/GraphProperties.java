e6c19846afe7a8573f80f911c93f85ea7baa25f1
James Cook <james.cook@utoronto.ca>
2012-03-07 17:29:19 -0800

    GraphProperties: KNN Similarities.

--- src/recommend/analysis/GraphProperties.java
+++ src/recommend/analysis/GraphProperties.java
@@ -1,5 +1,6 @@
 package recommend.analysis;
 
+import cern.colt.list.*;
 import cern.colt.matrix.*;
 import cern.colt.matrix.impl.*;
 import cern.colt.matrix.linalg.Algebra;
@@ -81,18 +82,16 @@ public class GraphProperties {
         return v.zDotProduct(v);
     }
 
-    List<Double> randomSimilarities(DoubleMatrix2D docTerm, int n) {
-        int numDocs = docTerm.rows();
-        List<Double> l2Sims = new ArrayList<Double>();
+    List<Double> randomSimilarities(DoubleMatrix2D similarities, int n) {
+        int numDocs = similarities.rows();
+        List<Double> samples = new ArrayList<Double>();
         for (int i = 0; i < n; ++i) {
             int idx0 = rand.nextInt(numDocs);
             int idx1 = rand.nextInt(numDocs - 1);
             if (idx0 <= idx1) ++idx1;
-            DoubleMatrix1D d0 = docTerm.viewRow(idx0);
-            DoubleMatrix1D d1 = docTerm.viewRow(idx1);
-            l2Sims.add(d0.zDotProduct(d1) / Math.sqrt(normL2Sqr(d0) * normL2Sqr(d1)));
+            samples.add(similarities.get(idx0, idx1));
         }
-        return l2Sims;
+        return samples;
     }
 
     int sampleUnnorm(DoubleMatrix1D freqs, double l1) {
@@ -114,6 +113,37 @@ public class GraphProperties {
             }
         }
         return docTerm;
+    }
+
+    DoubleMatrix2D normalizeRowsL2(DoubleMatrix2D m) {
+        DoubleMatrix2D mNorm = m.like(m.rows(), m.columns());
+        for (int r = 0; r < m.rows(); ++r) {
+            DoubleMatrix1D row = m.viewRow(r);
+            double rowNorm = Math.sqrt(row.zDotProduct(row));
+            IntArrayList indexList = new IntArrayList();
+            DoubleArrayList valueList = new DoubleArrayList();
+            row.getNonZeros(indexList, valueList);
+            for (int i = 0; i < indexList.size(); ++i) {
+                mNorm.set(r, indexList.get(i), valueList.get(i) / rowNorm);
+            }
+        }
+        return mNorm;
+    }
+
+    DoubleMatrix2D rowSimilaritiesL2(DoubleMatrix2D m) {
+        DoubleMatrix2D mNorm = normalizeRowsL2(m);
+        return mNorm.zMult(mNorm, null, 1.0, 0.0, false, true);
+    }
+
+    List<Double> knnSimilarities(DoubleMatrix2D sim, int numNearest) {
+        List<Double> ret = new ArrayList<Double>();
+        for (int i = 0; i < sim.rows(); ++i) {
+            DoubleMatrix1D nearest = sim.viewRow(i).viewSorted().viewFlip();
+            // Ignore the first element, which is the document's similarity with itself.
+            for (int j = 1; j <= numNearest; ++j)
+                ret.add(nearest.get(j));
+        }
+        return ret;
     }
 
     void dynMain(String[] argv) throws Throwable {
@@ -139,11 +169,20 @@ public class GraphProperties {
         report("Sorted term frequencies:");
         reportPlot(sortedTermFreqs, true, true);
 
-        /* Experiment: typical similarities. */
-        report("Here are some L2-normalized similarities of random pairs of distinct documents.");
-        reportHistogram(randomSimilarities(docTerm, 10000), 100);
+        /* Experiment: random similarities. */
+        DoubleMatrix2D docSimilaritiesL2 = rowSimilaritiesL2(docTerm);
+        report("Here are some L2-normalized similarities of 10000 random pairs of distinct documents.");
+        reportHistogram(randomSimilarities(docSimilaritiesL2, 10000), 100);
+        DoubleMatrix2D indDocTerm = genIndependentDocs(sortedTermFreqs, numDocs, 50);
+        DoubleMatrix2D indSimL2 = rowSimilaritiesL2(indDocTerm);
         report("The same experiment, but with a set of " + numDocs + " random documents with 50 words each.");
-        reportHistogram(randomSimilarities(genIndependentDocs(sortedTermFreqs, numDocs, 50), 10000), 100);
+        reportHistogram(randomSimilarities(indSimL2, 10000), 100);
+
+        /* Experiment: similarities of nearest neighbours. */
+        report("Similarities of each true document's 50 nearest neighbours.");
+        reportHistogram(knnSimilarities(docSimilaritiesL2, 50), 100);
+        report("Similarities of each synthetic document's 50 nearest neighbours.");
+        reportHistogram(knnSimilarities(indSimL2, 50), 100);
 
         endReport();
     }
