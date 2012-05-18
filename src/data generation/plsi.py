e885a4f884603f9b448f61bf9d0a6f41e17562fe
vortic <vortic@berkeley.edu>
2012-05-17 18:45:07 -0700

    added more output to plsi.py
    plsi.py now outputs model distributions and supports lda functionality

--- src/data generation/plsi.py
+++ src/data generation/plsi.py
@@ -5,6 +5,7 @@ from lda import normalize
 
 import numpy as np
 from numpy import array
+from numpy.random.mtrand import dirichlet
 
 import random
 from random import random as rand
@@ -12,17 +13,21 @@ from random import randint as rint
 from random import sample as rsample
 
 def generate_ratings(num_types, num_users, ratings_per_user=20, num_items=100,
-                     noise=0):
+                     alpha=None, noise=0, plsi=False):
     p = Poisson(ratings_per_user)
     ratings = [[rint(1,5) for i in range(num_items)] for i in range(num_types)]
-    
+    if alpha == None:
+        alpha = [1]*num_types
     user_ratings = []
     user_indices = []
     type_dists = []
     for i in range(num_users):
         ratings_per_user = p.sample()
-        type_dist = normalize(array([rand() for t in range(num_types)], 
-                                    'double'))
+        if plsi:
+            type_dist = normalize(array([rand() for t in range(num_types)], 
+                                        'double'))
+        else:
+            type_dist = dirichlet(alpha)
         type_dists.append(type_dist)
         rating = []
         indices = []
@@ -35,8 +40,6 @@ def generate_ratings(num_types, num_users, ratings_per_user=20, num_items=100,
             rating.append(ratings[type][j])
             indices.append(j)
         additions = int((noise / 100.0) * ratings_per_user)
-        print "ratings", ratings_per_user
-        print "additions", additions
         for k in rsample(items_left, additions):
             rating.append(rint(1,5)) 
         user_ratings.append(rating)
@@ -45,7 +48,8 @@ def generate_ratings(num_types, num_users, ratings_per_user=20, num_items=100,
     
     return user_ratings, ratings, type_dists
 
-def write(user_ratings):
+def write(data):
+    user_ratings, ratings, types = data
     user_indices, user_ratings = user_ratings
     with open('ratings-out', 'w') as f:
         for i in range(len(user_indices)):
@@ -55,10 +59,20 @@ def write(user_ratings):
             for rating in user_ratings[i]:
                 f.write(str(rating) + " ")
             f.write('\n')
+    with open('ratings_model-out', 'w') as f:
+        for user in types:
+            for type in user:
+                f.write(str(type) + " ")
+            f.write('\n')
+        f.write("V\n")
+        for user_type in ratings:
+            for item in user_type:
+                f.write(str(item) + " ")
+            f.write('\n')
 
 def main():
     data = generate_ratings(4, 20)
-    write(data[0])
+    write(data)
     return data
 
 if __name__ == '__main__':
