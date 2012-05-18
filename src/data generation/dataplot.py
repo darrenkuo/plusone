8c53303903fdd39d6c019c31d041883f21f0846c
vortic <vortic@berkeley.edu>
2012-05-18 14:24:57 -0700

    file created
    added script to plot things using ipython -pylab

---
+++ src/data generation/dataplot.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib.pyplot import *
+import pylab
+
+def plot_types(types):
+    offset = 0
+    for type in types:
+        bar(offset, type, 0.01)
+        offset += 0.01
+    xticks(np.arange(.005, .1, .01), range(10))
