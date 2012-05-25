7f35b04adc2702c22cc51ac116e83420e077c611
Andrew Gambardella <andrewgambardella18902@gmail.com>
2012-05-24 23:15:29 -0700

    MakeJSON should work for regression tasks too now. Had to rename data generation because otherwise eclipse could not run it

--- src/datageneration/MakeJSON.java
+++ src/datageneration/MakeJSON.java
@@ -1,15 +1,13 @@
-package recommend.data;
+package datageneration;
 
 import java.io.*;
 import java.util.*;
 
 import org.json.*;
 
-import recommend.data.MovieLens.Pair;
-
-/** Makes a JSON file corresponding to a file given by the string on the first line of the main method. If regression is true,
+/** Makes a JSON file corresponding to a file given by args[0]. If args[1] is "true",
  *  the input will have the (integer) indices of items on each odd numbered line, with each item's score on the next even
- *  numbered line. If regression is false, the each line of the input will correspond to a different document and each
+ *  numbered line. If args[1] is "false", the each line of the input will correspond to a different document and each
  *  number will be the items in the document. Consider the input:
  *  1 3 4
  *  2 2 5
@@ -25,14 +23,13 @@ public class MakeJSON {
 	public static void main(String[] args) throws IOException, JSONException {
     	PrintWriter out = new PrintWriter( new BufferedWriter( new FileWriter( "out2.json" ) ) );
 		
-		//Needs to be changed depending on where file is located
-		//String thisfile = args[0];
-		String thisfile = "/Users/andrewgambardella/Downloads/documents-out";
+    	//Location of the file
+		String thisfile = args[0];
 		//Set to true if each item has a corresponding score, false otherwise
 		boolean regression = false;
-		/*if (args[1].equals("true")) {
+		if (args[1].equals("true")) {
 			regression = true;
-		}*/
+		}
 		File fixedFile = null;
 		try {
 			fixedFile = preprocess(thisfile.toString());
@@ -49,9 +46,6 @@ public class MakeJSON {
 			System.exit(1);
 		}
 		
-		int count = 0;
-		int numUsers = 0;
-		int numItems = 0;
 		if (regression) {
 		    HashMap<Integer,ArrayList<Pair>> hm = new HashMap<Integer,ArrayList<Pair>>();
 			int users = 0;
@@ -82,12 +76,15 @@ public class MakeJSON {
 		    	JSONObject user = new JSONObject();
 		    	user.put( "id",  name );
 		    	JSONArray itemsJSON = new JSONArray();
+		    	JSONArray scoresJSON = new JSONArray();
 		    	
-		    	for( Integer a : hm.get( name ) ) {
-		    		itemsJSON.put( seenItems.indexOf(a.toString()) );
+		    	for( Pair a : hm.get( name ) ) {
+		    		itemsJSON.put( seenItems.indexOf(a.movie + "") );
+		    		scoresJSON.put( a.rating );
 		    	}
 		    	
 		    	user.put( "items", itemsJSON );
+		    	user.put( "scores", scoresJSON );
 		    	userArray[name] = user;
 		    }
 
