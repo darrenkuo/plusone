688db03ef106e8ba511044a6f9ee5e02795527dc
vortic <vortic@berkeley.edu>
2012-05-30 15:53:08 -0700

    MakeJSON.java now takes command line arguments

--- src/datageneration/MakeJSON.java
+++ src/datageneration/MakeJSON.java
@@ -21,11 +21,11 @@ import org.json.*;
  */
 public class MakeJSON {
 	public static void main(String[] args) throws IOException, JSONException {
-    	PrintWriter out = new PrintWriter( new BufferedWriter( new FileWriter( "out2.json" ) ) );
+    	PrintWriter out = new PrintWriter( new BufferedWriter( 
+    			new FileWriter( "data/out2.json" ) ) );
 		
     	//Location of the file
-		//String thisfile = args[0];
-    	String thisfile = "/Users/andrewgambardella/Downloads/documents-out";
+		String thisfile = args[0];
 		//Set to true if each item has a corresponding score, false otherwise
 		boolean regression = false;
 		/*if (args[1].equals("true")) {
